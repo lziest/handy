bcce992b9de638db2730957084e82d76c0630ad6
Zi Lin <zi@cloudflare.com>
2016-09-15 16:54:23 -0700

    first commit: sorted string methods

---
+++ sortedstrings.go
@@ -0,0 +1,51 @@
+package handy
+
+import (
+	"fmt"
+)
+
+// SortedStrings stores strings, assumed sorted.
+type SortedStrings []string
+
+// Minus returns all words in ss that is not in yass. In
+// other words, it is ss - yass.
+func (ss SortedStrings) Minus(yass SortedStrings) []string {
+	ret := []string{}
+	l, r := 0, len(yass)
+	for _, word := range ss {
+		// search through ss
+		region := yass[l:r]
+		index := region.BinarySearch(word)
+
+		fmt.Println(index)
+		fmt.Println(word)
+		fmt.Println(region)
+		if index >= len(region) || word != region[index] {
+			ret = append(ret, word)
+		}
+		l = index
+	}
+
+	return ret
+}
+
+// BinarySearch finds word in the sorted string list, if
+// found, it returns the index, otherwise, it returns the
+// index where the word can be inserted without breaking ordering.
+func (ss SortedStrings) BinarySearch(word string) int {
+	l, r := 0, len(ss)-1
+	for l >= 0 && r < len(ss) && l <= r {
+		mid := (l + r) >> 1
+		if ss[mid] == word {
+			return mid
+		} else {
+			if ss[mid] > word {
+				r = mid - 1
+			} else {
+				l = mid + 1
+			}
+		}
+	}
+	// This takes a little small proof why doing so.
+	return l
+}
