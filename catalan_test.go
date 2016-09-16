271c5433d0a5119b47d99a9f82adbd17e40d615f
Zi Lin <zi@cloudflare.com>
2016-09-16 15:24:29 -0700

    add calatan number computation

---
+++ catalan_test.go
@@ -0,0 +1,48 @@
+package handy
+
+import (
+	"math/big"
+	"testing"
+)
+
+func TestCalatan(t *testing.T) {
+	type testcase struct {
+		n int
+		c *big.Int
+	}
+
+	testcases := []testcase{
+		{
+			n: 0,
+			c: big.NewInt(1),
+		},
+		{
+			n: 1,
+			c: big.NewInt(1),
+		},
+		{
+			n: 2,
+			c: big.NewInt(2),
+		},
+		{
+			n: 3,
+			c: big.NewInt(5),
+		},
+		{
+			n: 4,
+			c: big.NewInt(14),
+		},
+		{
+			n: 20,
+			c: big.NewInt(6564120420),
+		},
+	}
+
+	for _, c := range testcases {
+		r := CatalanNumber(c.n)
+		if r.Cmp(c.c) != 0 {
+			t.Fatal("bad Catalan number: %v, got %v, expected %v", c.n, r, c.c)
+		}
+
+	}
+}
